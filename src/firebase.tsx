50c37068385be374281c760b55caa7175accbfd5
F13b <p.a.kononenkov@gmail.com>
2023-02-13 17:26:30 +0300

    create app

---
+++ src/firebase.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "simple-todo-app-48ca3.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: "simple-todo-app-48ca3.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
